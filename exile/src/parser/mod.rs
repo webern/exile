ef9a7267d5e382bad1e03566de40042bbba601c8
Matthew James Briggs <matthew.james.briggs@gmail.com>
2020-07-18 19:59:51 -0700

    cargo fix

--- exile/src/parser/mod.rs
+++ exile/src/parser/mod.rs
@@ -4,7 +4,7 @@ use std::str::Chars;
 
 use xdoc::{Declaration, Document, Encoding, Misc, Version};
 
-use crate::error::{display_char, Error, parse_err, ParseError, Result, ThrowSite, XMLSite};
+use crate::error::{display_char, parse_err, Error, ParseError, Result, ThrowSite, XMLSite};
 use crate::parser::chars::{is_name_char, is_name_start_char};
 use crate::parser::element::parse_element;
 use crate::parser::pi::{parse_pi, parse_pi_logic};
