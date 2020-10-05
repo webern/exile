fcaff33ed1eb643c3be4f29a29245ec782a7525f
Matthew James Briggs <matthew.james.briggs@gmail.com>
2020-10-04 17:02:30 -0700

    java project

---
+++ java/xmltestgen/src/main/java/com/matthewjamesbriggs/xmltestgen/App.java
@@ -0,0 +1,9 @@
+package com.matthewjamesbriggs.xmltestgen;
+
+import org.apache.xerces.parsers.DOMParser;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
