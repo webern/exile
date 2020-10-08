3b42423547ef2205ee70fa2c6898d147233f2baa
Matthew James Briggs <matthew.james.briggs@gmail.com>
2020-10-07 20:22:13 -0700

    load the xmlconf file

--- java/xmltestgen/src/main/java/com/matthewjamesbriggs/xmltestgen/App.java
+++ java/xmltestgen/src/main/java/com/matthewjamesbriggs/xmltestgen/App.java
@@ -11,18 +11,38 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.FactoryConfigurationError;
 import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
 
 public class App {
     private static Object SAXException;
 
     public static void main(String[] args) {
-        final String xmlFile = "file:///Users/mjb/repos/mx/Documents/musicxml.xsd";
+
+        if (args.length != 2) {
+            System.err.println("--testdata path is required.");
+            System.out.println("Example usage: ./testgen --testdata /path/to/dir");
+            System.exit(255);
+        }
+        String p = args[1];
+        File f = new File(System.getProperty("user.dir"), p);
+        String dir = "";
+        try {
+            dir = f.getCanonicalPath();
+        } catch (IOException e) {
+            System.out.println(String.format("Unable to find directory %s from parent %s", p, System.getProperty("user.dir")));
+            System.exit(254);
+        }
+        String path = String.format("%s/xmlconf/xmlconf.xml", dir);
+        String uri = String.format("file://%s", path);
+
+        String pathToTestData = String.format("file://%s/xmlconf/xmlconf.xml", p);
         try {
             DocumentBuilderFactory factory =
                     DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
-            Document document = builder.parse(xmlFile);
+            Document document = builder.parse(uri);
             NodeList childNodes = document.getChildNodes();
             if (childNodes.getLength() != 1) {
                 System.err.println("No root node.");
