20c6fc04dca5e841975dad667fd6abb48df56a0e
Matthew James Briggs <matthew.james.briggs@gmail.com>
2020-10-17 10:49:35 -0700

    some thoughts

--- exile/tests/generated/valid_sa_005.rs
+++ exile/tests/generated/valid_sa_005.rs
@@ -1,5 +1,7 @@
 // generated file, do not edit
+use std::io::Cursor;
 use std::path::PathBuf;
+
 const MANIFEST_DIR: &str = env!("CARGO_MANIFEST_DIR");
 const INPUT_DATA: &str = "input_data";
 const FILENAME: &str = "jclark_valid_sa_005.xml";
@@ -16,5 +18,10 @@ fn path() -> PathBuf {
 #[test]
 fn valid_sa_005() {
     let path = path();
-    let _doc = exile::load(&path).unwrap();
+    let original = exile::load(&path).unwrap();
+    let mut buff = Cursor::new(Vec::new());
+    original.write(&mut buff).unwrap();
+    let data = buff.into_inner();
+    let data = std::str::from_utf8(data.as_slice()).unwrap();
+    assert_eq!(data, r#""#);
 }
