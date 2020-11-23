7d8ca8907752e0aa786c5693fbad812b254ab24d
Matthew James Briggs <matthew.james.briggs@gmail.com>
2020-11-22 17:02:09 -0800

    delete comments

--- src/xdoc/chars.rs
+++ src/xdoc/chars.rs
@@ -2,13 +2,3 @@
 pub fn is_whitespace(c: char) -> bool {
     c == ' ' || c == '\t' || c == '\r' || c == '\n'
 }
-
-// contains `(#x20 | #x9 | #xD | #xA)`, i.e. space, tab, carriage return, or line feed.
-// pub fn contains_whitespace<S: AsRef<str>>(s: S) -> bool {
-//     for c in s.as_ref().chars() {
-//         if is_whitespace(c) {
-//             return true;
-//         }
-//     }
-//     false
-// }
